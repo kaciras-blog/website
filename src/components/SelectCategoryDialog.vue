9e51acac43f41ad9adeaaae5c25f14f9b140e085
Kaciras <kaciras@outlook.com>
2018-09-02 19:08:25 +0800

    add SelectCategoryDialog.

---
+++ src/components/SelectCategoryDialog.vue
@@ -0,0 +1,28 @@
+<template>
+<kx-dialog class="category-selector" :draggable="true">
+	<h3 slot="title">选择分类</h3>
+	<category-view></category-view>
+</kx-dialog>
+</template>
+
+<script>
+import CategoryView from "./CategoryView";
+
+export default {
+	name: "SelectCategoryDialog",
+	components: {CategoryView},
+};
+</script>
+
+<style lang="less">
+.category-selector {
+
+	& .kx-dialog-body {
+		padding: 0;
+	}
+	& .kx-dialog {
+		width: 60%;
+		height: 80%;
+	}
+}
+</style>
