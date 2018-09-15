d3bad21f65b40a9aba2367ae6f4446d62afc1d3b
Kaciras <kaciras@outlook.com>
2018-09-15 13:50:08 +0800

    optimize rank list style.

--- src/components/AsidePanel.vue
+++ src/components/AsidePanel.vue
@@ -2,15 +2,16 @@
 	<aside class="panel">
 		<div>
 			<h3 class="padding">置顶</h3>
-			<p style="text-align: center">暂无</p>
+			<p class="no-content">暂无</p>
 		</div>
 		<div>
 			<h3 class="padding">浏览排行</h3>
-			<ol class="rank">
-				<li v-for="article in hotArticles" :key="article.id">
-					<a target='_Blank' :href="'/article/' + article.id">{{article.title}}</a>
+			<ol class="rank" v-if="hots">
+				<li v-for="article in hots" :key="article.id">
+					<router-link class="item" :to="'/article/' + article.id">{{article.title}}</router-link>
 				</li>
 			</ol>
+			<p class="no-content" v-else>暂无</p>
 		</div>
 	</aside>
 </template>
@@ -22,18 +23,21 @@ export default {
 	name: "aside-panel",
 	data() {
 		return {
-			hotArticles: [],
+			hots: null,
 		};
 	},
 	created() {
-		api.recommend.getHotArticles().then(hots => this.hotArticles = hots);
+		api.recommend.getHotArticles().then(hots => this.hots = hots);
 	},
 };
 </script>
 
-<style scoped lang="less">
+<style lang="less">
 .padding {
 	padding-top: .2em;
 	padding-bottom: .2em;
+}
+.no-content {
+	text-align: center;
 }
 </style>
