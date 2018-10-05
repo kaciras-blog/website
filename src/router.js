c55c2a55065bfb2c0fa8021536592d360ca35551
Kaciras <kaciras@outlook.com>
2018-10-05 14:57:29 +0800

    Router: add scroll control.

--- src/router.js
+++ src/router.js
@@ -5,6 +5,34 @@ import Welcome from "./views/welcome/Welcome";
 
 Vue.use(Router);
 
+/**
+ * 控制路由切换后的滚动位置，默认行为跟原生浏览器一致，通过设置路由元信息 meta
+ * 的 retainScroll 选项可以跨页面保持位置。
+ *
+ * 抄自官方示例：
+ * @link https://github.com/vuejs/vue-router/blob/next/examples/scroll-behavior/app.js
+ *
+ * @param to 原页面的路由对象
+ * @param from 目标页面的路由对象
+ * @param savedPosition 浏览器的[前进/后退]按钮触发时，上次页面的位置
+ * @return {{ x: number, y: number }} 滚动的位置
+ */
+function scrollBehavior(to, from, savedPosition) {
+	if (savedPosition) {
+		return savedPosition;
+	} else {
+		const position = {};
+		if (to.hash) {
+			position.selector = to.hash;
+		}
+		if (!to.matched.some(m => m.meta.retainScroll)) {
+			position.x = 0;
+			position.y = 0;
+		}
+		return position;
+	}
+}
+
 /*
  * 两种情况下使用异步组件：
  * 1.不常被访问的页面，如编辑器、控制台。
@@ -68,5 +96,5 @@ export default function () {
 		routes.unshift({ path: "/test", component: () => import("./views/Test") });
 	}
 
-	return new Router({ mode: "history", routes });
+	return new Router({ mode: "history", scrollBehavior, routes });
 }
