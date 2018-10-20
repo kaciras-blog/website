2a046a27336169974c92eb61c88c81db34e5b120
Kaciras <kaciras@outlook.com>
2018-10-20 17:26:57 +0800

    polish.

--- build/webpack.server.conf.js
+++ build/webpack.server.conf.js
@@ -22,21 +22,20 @@ module.exports = merge(baseConfig, {
 			extract: false,
 		}),
 	},
-	// https://webpack.js.org/configuration/externals/#function
-	// https://github.com/liady/webpack-node-externals
-	// 外置化应用程序依赖模块。可以使服务器构建速度更快，
-	// 并生成较小的 bundle 文件。
+
+	// 外置化应用程序依赖模块。可以使服务器构建速度更快，并生成较小的 bundle 文件。
 	externals: nodeExternals({
 		whitelist: [/\.css$/, /\.less$/, /\.vue$/, /kx-ui/],
 	}),
 
-	// 这是将服务器的整个输出
-	// 构建为单个 JSON 文件的插件。
-	// 默认文件名为 `vue-ssr-server-bundle.json`
+
 	plugins: [
 		new webpack.DefinePlugin({
 			"process.env.VUE_ENV": "'server'",
 		}),
+
+		// 这是将服务器的整个输出构建为单个 JSON 文件的插件。
+		// 默认文件名为 `vue-ssr-server-bundle.json`
 		new VueSSRServerPlugin(),
 	],
 });
