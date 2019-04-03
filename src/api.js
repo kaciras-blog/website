956364795a502203da26decabb245c6081c5fc9a
Kaciras <Kaciras@outlook.com>
2019-04-03 17:19:36 +0800

    api: remove http2 adapter

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import * as KxUI from "kx-ui/src/helpers";
+import * as KxUI from "kx-ui";
 
 const CSRF_COOKIE_NAME = "CSRF-Token";
 const CSRF_HEADER_NAME = "X-CSRF-Token";
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
  *
  * @return {*} 配置选项
  */
-function defineApiServerConfig () {
+function defineApiServerConfig() {
 	const local = {
 		main: "https://localhost:2375",
 		front: "https://localhost",
@@ -32,34 +32,8 @@ function defineApiServerConfig () {
 	return productionWeb;
 }
 
-function request (options, callback) {
-	let host = `https://${options.hostname}`;
-	if (options.port) {
-		host += ":" + options.port;
-	}
-
-	const client = eval("require")("http2").connect(host);
-	const req = client.request({
-		...options.headers,
-		":method": options.method.toUpperCase(),
-		":path": options.path,
-	});
-
-	req.on("response", headers => {
-		req.headers = headers;
-		req.statusCode = headers[":status"];
-		callback(req);
-	});
-	req.on("end", () => client.close());
-	return req;
-}
-
-if (process.env.VUE_ENV === "server") {
-	Axios.defaults.transport = { request };
-}
-
 // MDZZ，axios不能全局配置拦截？
-function createAxios (config) {
+function createAxios(config) {
 	const instance = Axios.create(config);
 	instance.interceptors.response.use(null, error => {
 		if (error.response) {
@@ -91,13 +65,13 @@ const apiSet = {
  * @param status {number} 状态码
  * @return {boolean} 是否算作正常回复
  */
-function NormalResponse (status) {
+function NormalResponse(status) {
 	return status >= 0 && status < 500;
 }
 
 export class BasicApiFactory {
 
-	constructor (axiosSet) {
+	constructor(axiosSet) {
 		this.axiosSet = axiosSet;
 	}
 
@@ -108,7 +82,7 @@ export class BasicApiFactory {
 	 * @param proto 原请求
 	 * @return {*} API集
 	 */
-	withPrototype (proto) {
+	withPrototype(proto) {
 		return new ProxiedApiFactory(this.axiosSet).withPrototype(proto);
 	}
 
@@ -118,40 +92,40 @@ export class BasicApiFactory {
 	 * @param cancelToken { CancelToken } 取消令牌
 	 * @return {*} API集
 	 */
-	withCancelToken (cancelToken) {
+	withCancelToken(cancelToken) {
 		return new ProxiedApiFactory(this.axiosSet).withCancelToken(cancelToken);
 	}
 
 	// protected
-	createApiInstance (clazz) {
+	createApiInstance(clazz) {
 		return new clazz(this.axiosSet);
 	}
 
-	get article () {
+	get article() {
 		return this.createApiInstance(ArticleApi);
 	}
 
-	get category () {
+	get category() {
 		return this.createApiInstance(CategoryApi);
 	}
 
-	get discuss () {
+	get discuss() {
 		return this.createApiInstance(DiscussApi);
 	}
 
-	get draft () {
+	get draft() {
 		return this.createApiInstance(DraftApi);
 	}
 
-	get user () {
+	get user() {
 		return this.createApiInstance(UserApi);
 	}
 
-	get recommend () {
+	get recommend() {
 		return this.createApiInstance(RecommandApi);
 	}
 
-	get misc () {
+	get misc() {
 		return this.createApiInstance(MiscApi);
 	}
 }
@@ -159,11 +133,11 @@ export class BasicApiFactory {
 
 class AxiosSetProxy {
 
-	constructor (filters) {
+	constructor(filters) {
 		this.filters = filters;
 	}
 
-	get (target, name) {
+	get(target, name) {
 		return new Proxy(target[name], new AxiosProxy(this.filters));
 	}
 }
@@ -176,17 +150,17 @@ class AxiosSetProxy {
  */
 class AxiosProxy {
 
-	constructor (filters) {
+	constructor(filters) {
 		this.filters = filters;
 	}
 
-	prepare (config) {
+	prepare(config) {
 		config = config || {};
 		this.filters.forEach(f => f(config));
 		return config;
 	}
 
-	get (target, name) {
+	get(target, name) {
 		if (["delete", "get", "head", "options"].indexOf(name) > 0) {
 			return (url, config) => target[name](url, this.prepare(config));
 		}
@@ -199,16 +173,16 @@ class AxiosProxy {
 
 class ProxiedApiFactory extends BasicApiFactory {
 
-	constructor (axiosSet) {
+	constructor(axiosSet) {
 		super(axiosSet);
 		this.filters = [];
 	}
 
-	createApiInstance (clazz) {
+	createApiInstance(clazz) {
 		return new clazz(new Proxy(this.axiosSet, new AxiosSetProxy(this.filters)));
 	}
 
-	withPrototype (proto) {
+	withPrototype(proto) {
 		if (!proto) {
 			return this;
 		}
@@ -228,7 +202,7 @@ class ProxiedApiFactory extends BasicApiFactory {
 		return this;
 	}
 
-	withCancelToken (cancelToken) {
+	withCancelToken(cancelToken) {
 		if (!cancelToken) {
 			return this;
 		}
@@ -249,15 +223,15 @@ class ProxiedApiFactory extends BasicApiFactory {
 
 class AbstractApi {
 
-	constructor (axiosSet) {
+	constructor(axiosSet) {
 		this.axiosSet = axiosSet;
 	}
 
-	get mainServer () {
+	get mainServer() {
 		return this.axiosSet.mainServer;
 	}
 
-	get webServer () {
+	get webServer() {
 		return this.axiosSet.webServer;
 	}
 }
@@ -270,11 +244,11 @@ class ArticleApi extends AbstractApi {
 		}).then(r => r.data);
 	}
 
-	publish (data) {
+	publish(data) {
 		return this.mainServer.post("/articles", data).then(r => r.headers["location"]);
 	}
 
-	update (id, data) {
+	update(id, data) {
 		return this.mainServer.put("/articles/" + id, data).then(r => r.headers["location"]);
 	}
 
@@ -288,7 +262,7 @@ class ArticleApi extends AbstractApi {
 	 * @param params
 	 * @return {Promise<Array>} 文章基本信息列表
 	 */
-	getList (params) {
+	getList(params) {
 		params = Object.assign({
 			start: 0,
 			count: 20,
@@ -297,19 +271,19 @@ class ArticleApi extends AbstractApi {
 		return this.mainServer.get("/articles", { params }).then(r => r.data);
 	}
 
-	getHots () {
+	getHots() {
 		return this.mainServer.get("/recommendation/articles").then(r => r.data);
 	}
 
-	remove (id) {
+	remove(id) {
 		return this.mainServer.patch(`/articles/${id}`, { deletion: true });
 	}
 
-	restore (id) {
+	restore(id) {
 		return this.mainServer.patch(`/articles/${id}`, { deletion: false });
 	}
 
-	changeCategories (id, category) {
+	changeCategories(id, category) {
 		return this.mainServer.patch(`/articles/${id}`, { category });
 	}
 }
@@ -317,42 +291,42 @@ class ArticleApi extends AbstractApi {
 
 class CategoryApi extends AbstractApi {
 
-	getChildren (id) {
+	getChildren(id) {
 		return this.mainServer.get(`/categories/${id}/children`).then(r => r.data);
 	}
 
-	deleteOne (id) {
+	deleteOne(id) {
 		return this.mainServer.delete("/categories/" + id, { params: { tree: false } });
 	}
 
-	deleteTree (id) {
+	deleteTree(id) {
 		return this.mainServer.delete("/categories/" + id, { params: { tree: true } });
 	}
 
-	get (id, aggregate) {
+	get(id, aggregate) {
 		return this.mainServer.get("/categories/" + id, { params: { aggregate } }).then(r => r.data);
 	}
 
-	move (id, parent, treeMode) {
+	move(id, parent, treeMode) {
 		return this.mainServer.post("/categories/transfer", { params: { id, parent, treeMode } });
 	}
 
-	create (data, parent) {
+	create(data, parent) {
 		return this.mainServer.post("/categories/", data, { params: { parent } });
 	}
 
-	update (id, data) {
+	update(id, data) {
 		return this.mainServer.put("/categories/" + id, data);
 	}
 }
 
 class DiscussApi extends AbstractApi {
 
-	add (objectId, content) {
+	add(objectId, content) {
 		return this.mainServer.post("/discussions", { objectId, type: 0, content });
 	}
 
-	getList (objectId, start, count) {
+	getList(objectId, start, count) {
 		return this.mainServer.get("/discussions",
 			{ params: { objectId, type: 0, start, count } }).then(r => r.data);
 	}
@@ -365,7 +339,7 @@ class DiscussApi extends AbstractApi {
 	 * @param count {int} 每页数量
 	 * @return Promise<?> 回复列表
 	 */
-	getReplies (discuz, index, count) {
+	getReplies(discuz, index, count) {
 		return this.mainServer.get(`/discussions/${discuz}/replies`, {
 			params: { start: index * count, count },
 		}).then(r => r.data);
@@ -378,25 +352,25 @@ class DiscussApi extends AbstractApi {
 	 * @param content {String} 内容
 	 * @return Promise
 	 */
-	reply (discuz, content) {
+	reply(discuz, content) {
 		return this.mainServer.post(`/discussions/${discuz}/replies`, content, {
 			headers: { "Content-Type": "text/plain;charset=UTF-8" },
 		});
 	}
 
-	remove (id) {
+	remove(id) {
 		return this.mainServer.patch(`/discussions/${id}`, { deletion: true });
 	}
 
-	restore (id) {
+	restore(id) {
 		return this.mainServer.patch(`/discussions/${id}`, { deletion: false });
 	}
 
-	voteUp (id) {
+	voteUp(id) {
 		return this.mainServer.post(`/discussions/${id}/votes`);
 	}
 
-	revokeVote (id) {
+	revokeVote(id) {
 		return this.mainServer.delete(`/discussions/${id}/votes`);
 	}
 }
@@ -404,43 +378,43 @@ class DiscussApi extends AbstractApi {
 
 class DraftApi extends AbstractApi {
 
-	createNew () {
+	createNew() {
 		return this.mainServer.post("/drafts").then(resp => resp.headers["location"].substring("/drafts/".length));
 	}
 
-	fromArticle (article) {
+	fromArticle(article) {
 		return this.mainServer.post("/drafts", null, {
 			params: { article },
 		}).then(resp => resp.headers["location"].substring("/drafts/".length));
 	}
 
-	getList (userId, start, count) {
+	getList(userId, start, count) {
 		return this.mainServer.get("/drafts", { params: { userId, start, count } }).then(r => r.data);
 	}
 
-	get (id) {
+	get(id) {
 		return this.mainServer.get(`/drafts/${id}`).then(r => r.data);
 	}
 
-	getHistory (id, saveCount) {
+	getHistory(id, saveCount) {
 		return this.mainServer.get(`/drafts/${id}/histories/${saveCount}`).then(r => r.data);
 	}
 
-	saveNewHistory (id, data) {
+	saveNewHistory(id, data) {
 		return this.mainServer.post(`/drafts/${id}/histories`, data);
 	}
 
-	save (id, saveCount, data) {
+	save(id, saveCount, data) {
 		return this.mainServer.put(`/drafts/${id}/histories/${saveCount}`, data);
 	}
 
 	/** 删除一个草稿 */
-	remove (id) {
+	remove(id) {
 		return this.mainServer.delete("/drafts/" + id);
 	}
 
 	/** 清空所有草稿 */
-	clear () {
+	clear() {
 		return this.mainServer.delete("/drafts");
 	}
 }
@@ -453,7 +427,7 @@ class MiscApi extends AbstractApi {
 	 * @param progress 进度回调
 	 * @returns Promise<String>
 	 */
-	uploadImage (file, progress) {
+	uploadImage(file, progress) {
 		const data = new FormData();
 		data.append("file", file);
 		return this.webServer.post("/image", data, {
@@ -467,7 +441,7 @@ class MiscApi extends AbstractApi {
 	 *
 	 * @returns Promise<String> 保存的图片文件名
 	 */
-	async uploadImageFile () {
+	async uploadImageFile() {
 		const files = await KxUI.openFile(false, "image/*");
 		return await this.uploadImage(files[0]);
 	}
@@ -478,7 +452,7 @@ class MiscApi extends AbstractApi {
 	 *
 	 * @return {string} 验证码URL
 	 */
-	get captchaAddress () {
+	get captchaAddress() {
 		return apiConfig.main + "/captcha?r=" + Math.random();
 	}
 }
@@ -490,7 +464,7 @@ class UserApi extends AbstractApi {
 	 * 用户登录，登录成功后会添加相应的Cookie
 	 * @param form 一个对象，格式如下：{ name: 用户名, password: 密码, remember: 是否保存登录 }
 	 */
-	login (form) {
+	login(form) {
 		return this.mainServer.post("/accounts/login", form);
 	}
 
@@ -504,23 +478,23 @@ class UserApi extends AbstractApi {
 	 * }
 	 * @return Promise
 	 */
-	signup (data) {
+	signup(data) {
 		return this.mainServer.post("/accounts", data);
 	}
 
-	logout () {
+	logout() {
 		return this.mainServer.delete("/session/user");
 	}
 
-	getCurrent () {
+	getCurrent() {
 		return this.mainServer.get("/session/user", { validateStatus: NormalResponse });
 	}
 
-	get (id) {
+	get(id) {
 		return this.mainServer.get("/users/" + id);
 	}
 
-	updateProfile (profile) {
+	updateProfile(profile) {
 		return this.mainServer.patch("/session/user", profile);
 	}
 }
@@ -528,18 +502,18 @@ class UserApi extends AbstractApi {
 
 class RecommandApi extends AbstractApi {
 
-	get swiper () {
+	get swiper() {
 		return new SwiperApi(this.axiosSet);
 	}
 }
 
 class SwiperApi extends AbstractApi {
 
-	get () {
+	get() {
 		return this.mainServer.get("/recommendation/swiper").then(r => r.data);
 	}
 
-	set (list) {
+	set(list) {
 		return this.mainServer.put("/recommendation/swiper", list);
 	}
 }
