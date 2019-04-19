e34a2f92a2b80ad596cbba0df73c24c30ea7b245
Kaciras <Kaciras@outlook.com>
2019-04-19 20:44:28 +0800

    enable production sourcemap

--- config/webserver.prod.js
+++ config/webserver.prod.js
@@ -2,9 +2,8 @@ const config = require("./webserver");
 
 const { webpack, server, blog } = config;
 webpack.mode = "production";
-webpack.client.devtool = false;
+webpack.client.devtool = "source-map";
 webpack.client.cssSourceMap = false;
-webpack.server.devtool = false;
 
 delete server.httpsPort;
 server.certificate = "/etc/letsencrypt/live/blog.kaciras.net/cert.pem";
