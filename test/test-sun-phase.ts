9d14805a17003549c0b3dcec61fabe6ed0a15f08
Kaciras <Kaciras@outlook.com>
2019-10-03 18:02:25 +0800

    add more test cases in test-sun-phase

--- test/test-sun-phase.ts
+++ test/test-sun-phase.ts
@@ -8,52 +8,84 @@ function expectTimeout(time: number) {
 	jest.runOnlyPendingTimers();
 }
 
-it('should push phases', () => {
-	const data = new SunPhases({
-		first: 5,
-		second: 10,
-		third: 20,
-	});
-	const time = new Date('2019-01-13T02:20:25');
-	const results: string[] = [];
-	data.observe(time).subscribe(name => results.push(name));
-
-	expectTimeout(9575000);
-	expectTimeout(3600000 * 5);
-	expectTimeout(3600000 * 10);
-	expectTimeout(3600000 * 9);
-
-	jest.clearAllTimers();
-	expect(results).toStrictEqual(["third", "first", "second", "third", "first"]);
-});
-
-it('should fail on empty phases', () => {
+it('should fail on empty points', () => {
 	expect(() => new SunPhases({})).toThrow()
 });
 
-it('should work with only one phase', () => {
-	const results: string[] = [];
+describe('ofTime', () => {
 
-	new SunPhases({ first: 19 })
-		.observe()
-		.subscribe(name => results.push(name));
+	const data = new SunPhases({ one: 0, two: 12 });
 
-	jest.runOnlyPendingTimers();
-	expectTimeout(86400 * 1000);
-
-	expect(results).toStrictEqual(["first", "first", "first"]);
+	it('should return correct phase', () => {
+		expect(data.ofTime(new Date('2019-01-01T00:00:00'))).toBe("one");
+		expect(data.ofTime(new Date('2019-01-01T12:00:00'))).toBe("two");
+	});
 });
 
-it('should work on edge case', () => {
-	const data = new SunPhases({
-		first: 0,
-		second: 23,
-	});
-	const time = new Date('2019-01-13T00:00:00');
-	const results: string[] = [];
-	data.observe(time).subscribe(name => results.push(name));
+describe('nextOf', () => {
 
-	expectTimeout(3600000 * 23);
-	expectTimeout(3600000);
-	expect(results).toStrictEqual(["first", "second", "first"]);
+	const data = new SunPhases({ one: 0, two: 12 });
+
+	it('should return next phase', () => {
+		expect(data.nextOf("one")).toBe("two");
+		expect(data.nextOf("two")).toBe("one");
+	});
+
+	it('should throws on unknown name', () => {
+		expect(() => data.nextOf("zero")).toThrow()
+	});
+
+	it('should work with single phase', () => {
+		const data = new SunPhases({ one: 0 });
+		expect(data.nextOf("one")).toBe("one");
+	});
+});
+
+describe('observe', () => {
+
+	it('should push phases', () => {
+		const data = new SunPhases({
+			first: 5,
+			second: 10,
+			third: 20,
+		});
+		const time = new Date('2019-01-13T02:20:25');
+		const results: string[] = [];
+		data.observe(time).subscribe(name => results.push(name));
+
+		expectTimeout(9575000);
+		expectTimeout(3600000 * 5);
+		expectTimeout(3600000 * 10);
+		expectTimeout(3600000 * 9);
+
+		jest.clearAllTimers();
+		expect(results).toStrictEqual(["third", "first", "second", "third", "first"]);
+	});
+
+	it('should work with single phase', () => {
+		const results: string[] = [];
+
+		new SunPhases({ first: 19 })
+			.observe()
+			.subscribe(name => results.push(name));
+
+		jest.runOnlyPendingTimers();
+		expectTimeout(86400 * 1000);
+
+		expect(results).toStrictEqual(["first", "first", "first"]);
+	});
+
+	it('should work on edge case', () => {
+		const data = new SunPhases({
+			first: 0,
+			second: 23,
+		});
+		const time = new Date('2019-01-13T00:00:00');
+		const results: string[] = [];
+		data.observe(time).subscribe(name => results.push(name));
+
+		expectTimeout(3600000 * 23);
+		expectTimeout(3600000);
+		expect(results).toStrictEqual(["first", "second", "first"]);
+	});
 });
