eb6ebd1a1e736b2eed12b251bc6047f9cb9d228e
Kaciras <Kaciras@outlook.com>
2020-04-30 09:50:03 +0800

    feat: remove friend link

--- src/views/index/FriendsSection.vue
+++ src/views/index/FriendsSection.vue
@@ -3,17 +3,24 @@
 		<h1>友情链接</h1>
 		<ul class="clean-list" :class="$style.list">
 			<li
-				v-for="item of friends"
-				:key="item.id"
+				v-for="friend of friends"
+				:key="friend.id"
+				:class="$style.item"
 			>
-				<a :href="item.url" :class="$style.item">
+				<a :href="friend.url" class="clean-link">
 					<img
-						:src="item.favicon"
+						:src="friend.favicon"
 						alt="favicon"
 						:class="$style.favicon"
 					>
-					<div :class="$style.name">{{item.name}}</div>
+					<div :class="$style.name">{{friend.name}}</div>
 				</a>
+				<button
+					v-if="user.id === 2"
+					title="删除"
+					:class="$style.remove"
+					@click="() => rupture(friend)"
+				/>
 			</li>
 
 			<li
@@ -37,11 +44,14 @@
 import api from "@/api";
 import MakeFriendDialog from "@/views/index/MakeFriendDialog";
 import { mapState } from "vuex";
+import { deleteOn } from "@/utils";
 
 export default {
 	name: "FriendsSection",
 	data() {
-		return { friends: this.$store.state.prefetch.friends };
+		return {
+			friends: this.$store.state.prefetch.friends,
+		};
 	},
 	computed: mapState(["user"]),
 	methods: {
@@ -50,8 +60,16 @@ export default {
 			if (!result.isConfirm) {
 				return;
 			}
-			const newFriend = await api.friend.makeFriends(result.data);
-			this.friends.push(newFriend);
+			try {
+				const friend = await api.friend.makeFriends(result.data);
+				this.friends.push(friend);
+			} catch (e) {
+				this.$dialog.alertError(e.message);
+			}
+		},
+		async rupture(friend) {
+			await api.friend.rupture(friend);
+			deleteOn(this.friends, v => v === friend);
 		},
 	},
 };
@@ -64,9 +82,32 @@ export default {
 }
 
 .item {
-	composes: clean-link from global;
-	display: block;
+	position: relative;
 	padding: 20px;
+
+	&:hover, &:focus {
+		& > .remove {
+			opacity: 1;
+			transform: scale(1);
+		}
+	}
+}
+
+.remove {
+	position: absolute;
+	top: 10px;
+	right: 10px;
+	width: 30px;
+	height: 30px;
+	border-radius: 100%;
+
+	background-image: url("~@kaciras-blog/uikit/src/assets/icon-close.svg");
+	background-position: 50%;
+	opacity: 0;
+	cursor: pointer;
+
+	transform: scale(0.25);
+	transition: .3s;
 }
 
 .favicon {
