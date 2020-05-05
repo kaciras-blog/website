e4caea6bc962b943bc0b3cc0507f3cd8fdc16d1d
Kaciras <Kaciras@outlook.com>
2020-05-05 14:55:35 +0800

    cards endpoint changed

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -10,10 +10,10 @@ export interface Card {
 export default class CardResource extends AbstractResource {
 
 	getCards() {
-		return this.servers.content.get<Card[]>("/recommendation/cards").then(r => r.data);
+		return this.servers.content.get<Card[]>("/cards").then(r => r.data);
 	}
 
 	setCards(cards: Card[]) {
-		return this.servers.content.put("/recommendation/cards", cards);
+		return this.servers.content.put("/cards", cards);
 	}
 }
