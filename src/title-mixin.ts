29c597de5b5ab74d1e6b783ade827e39169a847b
Kaciras <Kaciras@outlook.com>
2020-06-07 19:48:24 +0800

    convert title-mixin.ts to typescript

--- src/title-mixin.ts
+++ src/title-mixin.ts
@@ -2,9 +2,11 @@
  * meta 由服务端注入，在客户端预载中并不关心这个字段，这也意味着客户端无法改变它。
  * 目前仅用来注入些SEO相关的信息，即便留到其它的页面也不会影响用户体验。
  */
+import Vue from "vue";
+
 const serverTitleMixin = {
-	created () {
-		let { title, metadata } = this.$options;
+	created(this: Vue) {
+		let { title, metadata } = this.$options as any;
 
 		if (typeof title === "function") {
 			title = title.call(this);
@@ -21,9 +23,10 @@ const serverTitleMixin = {
 	},
 };
 
+// 客户端渲染不需要关键字和描述
 const clientTitleMixin = {
-	beforeMount () {
-		let { title } = this.$options; // 关键字和描述就不管了
+	beforeMount(this: Vue) {
+		let { title } = this.$options as any;
 
 		if (typeof title === "function") {
 			title = title.call(this);
