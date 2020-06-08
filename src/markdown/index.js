d91d92f74fe7821d21de9dcb34d9d6cf9656eb89
Kaciras <Kaciras@outlook.com>
2020-06-08 19:18:55 +0800

    markdown: new image layout

--- src/markdown/index.js
+++ src/markdown/index.js
@@ -6,47 +6,7 @@ import tableOfContent from "markdown-it-toc-done-right";
 import katex from "@iktakahiro/markdown-it-katex";
 import media from "@kaciras-blog/server/lib/markdown-media";
 import lozad from "lozad";
-import loadingImage from "@/assets/img/loading.gif";
 import highlight from "./highlight";
-
-/**
- * 给行内代码加个 inline-code 类以便跟代码块区别开
- *
- * @param markdownIt MarkdownIt的实例
- */
-function inlineCodePlugin(markdownIt) {
-	markdownIt.core.ruler.push("inline_code_class", state => state.tokens
-		.filter(token => token.type === "inline")
-		.forEach(token => token.children
-			.filter(child => child.type === "code_inline")
-			.forEach(child => child.attrs = [["class", "inline-code"]])));
-}
-
-/**
- * 修改图片的渲染输出的插件，具有以下两个功能：
- * 1. 给外层加上链接并设置居中的class
- * 2. 将图片的src改为data-src以便懒加载。
- *
- * @param markdownIt MarkdownIt的实例
- */
-function lazyImagePlugin(markdownIt) {
-	const defaultImageRenderer = markdownIt.renderer.rules.image;
-
-	markdownIt.renderer.rules.image = (tokens, idx, options, env, self) => {
-		const token = tokens[idx];
-		const src = token.attrGet("src");
-		token.attrPush(["data-src", src]);
-		token.attrSet("src", loadingImage);
-
-		const img = defaultImageRenderer(tokens, idx, options, env, self);
-
-		return `
-			<span class="center-wrapper">
-				<a href="${src}" target="_blank">${img}</a>
-				<span class="md-alt">${token.content}</span>
-			</span>`;
-	};
-}
 
 export const converter = new MarkdownIt({
 	highlight: function (str, lang) {
@@ -74,8 +34,52 @@ converter.use(Anchor, {
 converter.use(media);
 converter.use(tableOfContent);
 converter.use(katex);
-converter.use(inlineCodePlugin);
-converter.use(lazyImagePlugin);
+
+/**
+ * 给行内代码加个 inline-code 类以便跟代码块区别开
+ */
+converter.core.ruler.push("inline_code_class", state => state.tokens
+	.filter(token => token.type === "inline")
+	.forEach(token => token.children
+		.filter(child => child.type === "code_inline")
+		.forEach(child => child.attrs = [["class", "inline-code"]])));
+
+/**
+ * 修改图片的渲染输出的插件，具有以下两个功能：
+ * 1. 给外层加上链接并设置居中的class
+ * 2. 将图片的src改为data-src以便懒加载。
+ */
+converter.renderer.rules.image = (tokens, idx) => {
+	const token = tokens[idx];
+	const src = token.attrGet("src");
+	const alt = token.content;
+
+	const urlParams = new URLSearchParams(src.split("?")[1]);
+	const vw = urlParams.get("vw");
+	const vh = urlParams.get("vh");
+
+	let style = "";
+	if (vw && vh) {
+		const ratio = parseInt(vh) / parseInt(vw) * 100;
+		style = `--width:${vw}px; --aspect-ratio:${ratio}%`;
+	}
+
+	return `
+		<span class="md-img-container" style="${style}">
+			<span class="full-vertex md-image-loading">
+    			<span class="dot"></span>
+    			<span class="dot"></span>
+    			<span class="dot"></span>
+    			<span class="dot"></span>
+    			<span class="dot"></span>
+    		</span>
+    		<a href="${src}" target="_blank" class="full-vertex">
+				<img data-src="${src}" alt="${alt}" class="md-img">
+			</a>
+    	</span>
+    	<span class="md-alt">${alt}</span>
+	`;
+};
 
 /**
  * 将 Markdown 渲染为 HTML.
@@ -94,7 +98,12 @@ export function renderMarkdown(markdown) {
  * @return 取消监听的函数，应当在被监视的内容被删除后调用，以避免内存泄漏。
  */
 export function enableLazyLoad(el) {
-	const images = lozad(el.querySelectorAll("img"));
+	const images = lozad(el.querySelectorAll("img"), {
+		loaded(el) {
+			el.removeAttribute("data-src");
+			// el.parentElement.previousElementSibling.remove();
+		},
+	});
 	images.observe();
 
 	const videos = new IntersectionObserver((entries) => {
