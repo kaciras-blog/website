2094609d5b54952cd00fc104ea464a5febac0158
Kaciras <Kaciras@outlook.com>
2020-07-26 13:39:52 +0800

    update deps

--- src/service-worker/api-cache.ts
+++ src/service-worker/api-cache.ts
@@ -30,7 +30,8 @@ class ApiOfflineRoute implements Route {
 			this.fetchFn = staleWhileRevalidate(this.cache);
 			console.info("[SW] API请求模式设置为StaleWhileRevalidate");
 		} else {
-			this.fetchFn = networkFirst(this.cache, timeout(7500));
+			const ms = process.env.TIMEOUT as unknown as number;
+			this.fetchFn = networkFirst(this.cache, timeout(ms));
 			console.info("[SW] API请求模式设置为NetworkFirst");
 		}
 	}
