76fd3624e223152eec98a2c1e6a276725233d56f
Kaciras <Kaciras@outlook.com>
2020-08-12 20:25:14 +0800

    entry-server: remove unnecessary function onReadyAsync()

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import { Store } from "vuex";
-import { Route, VueRouter } from "vue-router/types/router";
+import { Route } from "vue-router/types/router";
 import { CancellationToken } from "@kaciras-blog/uikit";
 import { SET_WIDTH } from "@kaciras-blog/uikit/src/media-query/index";
 import { configureForProxy } from "@kaciras-blog/server/lib/axios-helper";
@@ -29,16 +29,6 @@ ServerPrefetchContext.prototype.isServer = true;
 ServerPrefetchContext.prototype.cancelToken = CancellationToken.NEVER;
 
 /**
- * 等待路由准备好，都什么时代了 vue-router 还在用回调式的API。
- *
- * @param router Vue的路由
- * @return 在路由准备好了之后resolve
- */
-function onReadyAsync(router: VueRouter) {
-	return new Promise<void>((resolve, reject) => router.onReady(resolve, reject));
-}
-
-/**
  * 简单地通过 User-Agent 判断客户端的设备是不是手机
  *
  * @param userAgent User-Agent
@@ -59,7 +49,7 @@ export default async (context: RenderContext) => {
 	const ssrApi = api.withConfigProcessor(config => configureForProxy(request, config));
 
 	// 从 UserAgent 中检测是否手机，从而设定渲染的屏幕宽度
-	const userAgent = request && request.headers["user-agent"];
+	const userAgent = request?.headers["user-agent"];
 	if (userAgent && isMobile(userAgent)) {
 		store.commit(SET_WIDTH, mediaBreakpoints.mobile);
 	}
@@ -71,8 +61,9 @@ export default async (context: RenderContext) => {
 		await store.dispatch(REFRESH_USER, ssrApi);
 	}
 
-	router.push(url.pathname);
-	await onReadyAsync(router);
+	// router.push 返回的 Promise 等待所有 hooks 都调用完毕
+	await router.push(url.pathname);
+
 	const session = new ServerPrefetchContext(store, router.currentRoute, ssrApi);
 
 	/*
@@ -96,7 +87,7 @@ export default async (context: RenderContext) => {
 			case 404:
 			case 410:
 			case 429:
-				router.push("/error/" + e.code);
+				await router.push("/error/" + e.code);
 				break;
 			default:
 				throw e;
