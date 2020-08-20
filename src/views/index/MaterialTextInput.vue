9c41c4512dddf54579b10058f3fd83cb0972aeb0
Kaciras <Kaciras@outlook.com>
2020-08-20 22:06:50 +0800

    MaterialTextInput: fix input event handing

--- src/views/index/MaterialTextInput.vue
+++ src/views/index/MaterialTextInput.vue
@@ -3,15 +3,13 @@
 -->
 <template functional>
 	<label :class="$style.container">
-		<span :class="$style.label">
-			{{ props.label }}
-		</span>
+		<span :class="$style.label">{{ props.label }}</span>
 		<input
 			:class="$style.input"
 			:name="props.name"
 			:placeholder="props.placeholder"
 			:value="props.value"
-			@input="listeners.input"
+			@input="e => listeners.input(e.target.value)"
 		>
 	</label>
 </template>
@@ -40,14 +38,17 @@ export default {
 .input {
 	display: block;
 	width: 100%;
-
-	border-radius: 0;
-	border: none;
-	border-bottom: 2px solid #d0d0d0;
 	padding: 6px 0;
 
-	box-shadow: none !important;
+	border: none;
+	border-bottom: 2px solid #d0d0d0;
+	border-radius: 0;
+
 	transition: all .2s ease;
+
+	&:focus-within {
+		box-shadow: none;
+	}
 
 	&:focus {
 		border-bottom-color: #1175ff;
