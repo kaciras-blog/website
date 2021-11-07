5250ced4a05ac6642230434826a3d09917748eb7
Kaciras <Kaciras@protonmail.com>
2021-11-07 11:37:43 +0800

    use `@kaciras/eslint-config-*`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,14 @@
+const jestConfig = require("./jest.config");
+
 module.exports = {
 	root: true,
-	env: {
-		node: true
-	},
 	extends: [
-		"plugin:vue/essential",
-		"plugin:jest/recommended",
-		"eslint:recommended"
+		"@kaciras/core",
+		"@kaciras/typescript",
+		"@kaciras/vue/typescript",
 	],
-	rules: {
-		"indent": ["error", "tab", {
-			SwitchCase: 1,
-			FunctionDeclaration: { parameters: "off" }, // first 在 tab 缩进下有bug
-			ignoreComments: true, // IDE 自动注释直接加在开头
-		}],
-		"comma-dangle": ["error", "always-multiline"],
-		"space-before-function-paren": "off",
-		"semi": ["error", "always"],
-		"no-mixed-spaces-and-tabs": ["warn", "smart-tabs"],
-		"no-unused-vars": "warn",
-		"quotes": ["error", "double", { avoidEscape: true }],
-		"no-console": process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		"no-debugger": process.env.NODE_ENV === 'production' ? 'error' : 'off',
-	},
-	parserOptions: {
-		parser: 'babel-eslint'
-	}
+	overrides: [{
+		files: jestConfig.testMatch,
+		extends: ["@kaciras/jest"],
+	}],
 };
