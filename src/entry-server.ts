57b4ba3bc918c5f97f346edc1c68f477e87ec98c
Kaciras <Kaciras@protonmail.com>
2022-01-30 19:34:43 +0800

    refactor server entry

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,17 +1,16 @@
-import { createApp, h } from "vue";
+import { basename } from "path";
 import { Store } from "vuex";
 import { RouteLocationNormalizedLoaded } from "vue-router";
+import { renderToString, SSRContext } from 'vue/server-renderer';
 import { SET_WIDTH } from "@kaciras-blog/uikit";
 import { configureForProxy } from "@kaciras-blog/server/lib/axios-helper";
-import { RenderContext } from "@kaciras-blog/server/lib/koa/vue-ssr";
 import api, { Api } from "./api";
 import { PrefetchContext } from "./prefetch";
 import { REFRESH_USER, SET_PREFETCH_DATA } from "./store/types";
 import createBlogApp, { mediaBreakpoints } from "./main";
 
-/*
- *
- */
+const titleRE = new RegExp("<title>[^<]*</title>");
+const noSSR = new RegExp("^/(?:edit|console)/");
 
 // @ts-ignore isServer on prototype.
 class ServerPrefetchContext extends PrefetchContext {
@@ -41,38 +40,32 @@ ServerPrefetchContext.prototype.isServer = true;
  * 简单地通过 User-Agent 判断客户端的设备是不是手机
  *
  * @param userAgent User-Agent
- * @return 如果是手机返回true，否则false
+ * @return 如果是手机返回 true，否则 false
  */
 function isMobile(userAgent: string) {
 	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 }
 
-export default async (context: RenderContext) => {
-	const { request, url } = context;
+// noinspection JSUnusedGlobalSymbols 由服务器引用。
+export default async (context: any) => {
+	const { template, manifest, request, path } = context;
 
-	if (/^\/edit\//.test(url.pathname)) {
-		return createApp({ render: () => h("div", { id: "app" }) });
+	if (noSSR.test(path)) {
+		return template;
 	}
 
 	const { app, router, store } = createBlogApp();
 	const ssrApi = api.withConfigProcessor(config => configureForProxy(request, config));
 
-	// 从 UserAgent 中检测是否手机，从而设定渲染的屏幕宽度
-	const userAgent = request?.headers["user-agent"];
+	// 从 UserAgent 中检测是否手机，从而设定渲染的屏幕宽度。
+	const userAgent = request.headers["user-agent"];
 	if (userAgent && isMobile(userAgent)) {
 		store.commit(SET_WIDTH, mediaBreakpoints.mobile);
 	}
 
-	// 因为全站都是预渲染，所以初始用户在服务端加载一次即可。
-	// 控制台配置了拦截，必须先登陆，否则后面的路由直接跳到错误页
-	const userCheckNeeded = /^\/console\/?/.test(url.pathname);
-	if (userCheckNeeded) {
-		await store.dispatch(REFRESH_USER, ssrApi);
-	}
-
 	// router.push 返回的 Promise 等待所有 hooks 都调用完毕
-	router.push(url.pathname);
-	await router.isReady();
+	await router.push(path);
+	// await router.isReady();
 
 	const route = router.currentRoute.value;
 	const controller = new AbortController();
@@ -85,12 +78,10 @@ export default async (context: RenderContext) => {
 	 */
 	const tasks = route.matched
 		.flatMap(v => Object.values(v.components) as any)
-		.filter(c => c.loadData)
-		.map(c => c.loadData(session));
+		.filter(c => c.asyncData)
+		.map(c => c.asyncData(session));
 
-	if (!userCheckNeeded) {
-		tasks.push(store.dispatch(REFRESH_USER, ssrApi));
-	}
+	tasks.push(store.dispatch(REFRESH_USER, ssrApi));
 
 	try {
 		await Promise.all(tasks);
@@ -109,15 +100,80 @@ export default async (context: RenderContext) => {
 		}
 	}
 
+	const ssrContext: SSRContext = {
+		meta: "<meta name='description' content='欢迎来到 Kaciras 的博客'>"
+	};
+
 	const { title } = route.meta;
 	if (title) {
-		context.title = title + " - Kaciras的博客";
-		context.meta = "<meta name='description' content='欢迎来到Kaciras的博客'>";
+		ssrContext.title = `<title>${title} - Kaciras 的博客</title>`;
 	}
 
+	const appHtml = await renderToString(app, ssrContext);
 
-	// Vue2 的类型定义......
-	(context as any).state = store.state;
+	const initState = JSON.stringify(store.state);
+	ssrContext.meta += `<script>window.__INITIAL_STATE__=${initState}</script>`
 
-	return app;
+	const preloads = renderPreloadLinks(ssrContext.modules, manifest);
+
+	let result = template;
+
+	if (ssrContext.title) {
+		result = result.replace(titleRE, ssrContext.title);
+	}
+
+	return result
+		.replace("<!--ssr-metadata-->", ssrContext.meta ?? "")
+		.replace("<!--app-html-->", appHtml)
+		.replace("<!--preload-links-->", preloads);
 };
+
+/**
+ * https://github.com/vitejs/vite/blob/main/packages/playground/ssr-vue/src/entry-server.js
+ *
+ * @param modules
+ * @param manifest
+ */
+function renderPreloadLinks(modules: any, manifest: any) {
+	let links = "";
+	const seen = new Set();
+	modules.forEach((id: string) => {
+		const files = manifest[id];
+		if (files) {
+			files.forEach((file: string) => {
+				if (!seen.has(file)) {
+					seen.add(file);
+					const filename = basename(file);
+					if (manifest[filename]) {
+						for (const depFile of manifest[filename]) {
+							links += renderPreloadLink(depFile);
+							seen.add(depFile);
+						}
+					}
+					links += renderPreloadLink(file);
+				}
+			});
+		}
+	});
+	return links;
+}
+
+function renderPreloadLink(file: string) {
+	if (file.endsWith(".js")) {
+		return `<link rel="modulepreload" crossorigin href="${file}">`;
+	} else if (file.endsWith(".css")) {
+		return `<link rel="stylesheet" href="${file}">`;
+	} else if (file.endsWith(".woff")) {
+		return ` <link rel="preload" href="${file}" as="font" type="font/woff" crossorigin>`;
+	} else if (file.endsWith(".woff2")) {
+		return ` <link rel="preload" href="${file}" as="font" type="font/woff2" crossorigin>`;
+	} else if (file.endsWith(".gif")) {
+		return ` <link rel="preload" href="${file}" as="image" type="image/gif">`;
+	} else if (file.endsWith(".jpg") || file.endsWith(".jpeg")) {
+		return ` <link rel="preload" href="${file}" as="image" type="image/jpeg">`;
+	} else if (file.endsWith(".png")) {
+		return ` <link rel="preload" href="${file}" as="image" type="image/png">`;
+	} else {
+		return "";
+	}
+}
