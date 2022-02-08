fa154487d717c05e3cc77c55876ce9acbe3efc7d
Kaciras <Kaciras@protonmail.com>
2022-02-08 20:17:45 +0800

    inline about page chunks

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,18 @@ import { createMemoryHistory, createRouter, createWebHistory, RouteRecordRaw } f
 import IndexPage from "./views/index/IndexPage.vue";
 import ErrorPage from "./views/error/ErrorPage.vue";
 
+/*
+ * Vite 不支持自定义代码分割，如果自定义 manualChunks 的话也没法复用 Vite 内部的逻辑：
+ * https://github.com/vitejs/vite/blob/a0126441a556b4991ac14cf037820194ab9e17b9/packages/vite/src/node/build.ts#L619
+ *
+ * 如果用动态导入，关于页的子路由组件全会被分割，都是很小的文件完全不值得，如果能合并成一个的话还行。
+ */
+import AbortPage from "./views/about/AboutPage.vue";
+import AbortMe from "./views/about/Me.vue";
+import AbortFriends from "./views/about/Friends.vue";
+import AbortTechnology from "./views/about/Technology.vue";
+import AbortCopyrights from "./views/about/Copyrights.vue";
+
 const routes: RouteRecordRaw[] = [
 	{
 		path: "/",
@@ -43,26 +55,26 @@ const routes: RouteRecordRaw[] = [
 	},
 	{
 		path: "/about",
-		component: () => import("./views/about/AboutPage.vue"),
+		component: AbortPage,
 		children: [
 			{
 				path: "me",
-				component: () => import("./views/about/Me.vue"),
-				meta: { title: "博主" },
+				component: AbortMe,
+				meta: { title: "站长" },
 			},
 			{
 				path: "friends",
-				component: () => import("./views/about/Friends.vue"),
+				component: AbortFriends,
 				meta: { title: "友链" },
 			},
 			{
 				path: "technology",
-				component: () => import("./views/about/Technology.vue"),
+				component: AbortTechnology,
 				meta: { title: "技术栈" },
 			},
 			{
 				path: "copyright",
-				component: () => import("./views/about/Copyrights.vue"),
+				component: AbortCopyrights,
 				meta: { title: "版权声明" },
 			},
 		],
