f3a0ebe71b18c4ffb02185f80e84175a64d0ac23
Kaciras <Kaciras@protonmail.com>
2022-03-12 16:22:27 +0800

    KxDialogContainer renamed to DialogContainer

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 	<kx-progress :ref="connect"/>
 	<router-view/>
 	<ToastContainer/>
-	<kx-dialog-container/>
+	<dialog-container/>
 </template>
 
 <script setup lang="ts">
 import type { KxProgress } from "@kaciras-blog/uikit";
-import { useDialog, ToastContainer, KxDialogContainer } from "@kaciras-blog/uikit";
+import { DialogContainer, ToastContainer, useDialog } from "@kaciras-blog/uikit";
 import { Unsubscribe } from "nanoevents";
 import { events, PrefetchContext } from "./prefetch";
 import { useRouter } from "vue-router";
