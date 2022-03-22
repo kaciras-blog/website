d0cf1501db922f1ea9f31fd4c8a3a47d37ece8bc
Kaciras <Kaciras@protonmail.com>
2022-03-22 15:18:28 +0800

    fix usage of switch box

--- src/components/top-nav/SettingPanel.vue
+++ src/components/top-nav/SettingPanel.vue
@@ -1,9 +1,9 @@
 <template>
 	<kx-switch-box
 		:disabled="unsupported || loading"
-		:value="StaleApi"
+		:modelValue="StaleApi"
 		:class="$style.line"
-		@input="v => setOption('StaleApi', v)"
+		@update:modelValue="v => setOption('StaleApi', v)"
 	>
 		动态内容缓存
 	</kx-switch-box>
@@ -23,37 +23,36 @@
 	</div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
 import { KxSwitchBox } from "@kaciras-blog/uikit";
 import { getSettings, putSetting } from "@/service-worker/client/settings";
 
-export default {
+const unsupported = !("serviceWorker" in navigator);
+
+export default defineComponent({
 	name: "SettingPanel",
 	components: {
 		KxSwitchBox,
 	},
 	data: () => ({
+		unsupported,
 		loading: true,
 		error: null,
 		StaleApi: false,
 	}),
-	computed: {
-		unsupported() {
-			return !("serviceWorker" in navigator);
-		},
-	},
 	methods: {
-		async setOption(key, value) {
+		async setOption(key: string, value: any) {
 			await putSetting(key, value);
-			this[key] = value;
+			(this as any)[key] = value;
 		},
 	},
 	async beforeMount() {
-		if (this.unsupported) {
-			return; // SW 不是本项目必须的，故 navigator.serviceWorker 可能不存在
+		if (unsupported) {
+			return; // SW 不是本项目必须的，故 ServiceWorker 可能不存在
 		}
 
-		// 虽然说如果无法注册SW的话它永远不会resolve，但该组件用得不多，应该不至于内存泄漏。
+		// 虽然说如果无法注册 SW 的话它永远不会 resolve，但该组件用得不多，应该不至于内存泄漏。
 		await navigator.serviceWorker.ready;
 		try {
 			Object.assign(this.$data, await getSettings());
@@ -62,7 +61,7 @@ export default {
 			this.error = e.message;
 		}
 	},
-};
+});
 </script>
 
 <style module lang="less">
