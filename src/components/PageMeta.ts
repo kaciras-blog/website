201c110c72c7ce25d0b97c2d4c77a9815ab0b814
Kaciras <Kaciras@protonmail.com>
2022-04-07 21:27:03 +0800

    add comments to PageMeta.ts

--- src/components/PageMeta.ts
+++ src/components/PageMeta.ts
@@ -2,7 +2,15 @@ import type { SSRContext } from "vue/server-renderer";
 import { defineComponent, useSSRContext } from "vue";
 
 export interface PageMetaProps {
+
+	/**
+	 * 如果存在，则修改页面的标题。
+	 */
 	title?: string;
+
+	/**
+	 * 如果存在，则设为 body 元素的 class。
+	 */
 	bodyClass?: string;
 }
 
@@ -24,10 +32,34 @@ function renderClient(this: PageMetaProps) {
 	}
 }
 
+/**
+ * 修改一些全局 DOM 节点的组件，包括标题，body 的 class 等。
+ *
+ * 这是个非渲染组件，不影响 DOM，可以看做是 title 和一些属性的 teleport。
+ * 虽然放在任意位置的效果都一样，但推荐放在组件的开头。
+ *
+ * 注意该组件没有卸载机制，当页面切换时新的页必须也使用该组件来覆盖，
+ * 否则上一次设置的值仍留着。
+ *
+ * 当然也可以在一个页面中多次使用本组件，各自修改不同的属性。
+ *
+ * <h2>为什么用组件</h2>
+ * 从语义上看，标题和全局元素的 class 也属于 DOM 的一部分，用模板是合适的。
+ * 而且比起在 JS 里用各种钩子，模板渲染也正好是修改它们的时机。
+ *
+ * 这个类的灵感来自于 Next.js 的 Head 组件。
+ * https://nextjs.org/docs/api-reference/next/head
+ */
 export default defineComponent({
 	props: {
-		title: { type: String, required: false },
-		bodyClass: { type: String, required: false },
+		title: {
+			type: String,
+			required: false,
+		},
+		bodyClass: {
+			type: String,
+			required: false,
+		},
 	},
 	setup(props) {
 		if (import.meta.env.SSR) {
