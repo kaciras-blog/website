c598663cb897e4469fe98a94e4593863ee9e0e1e
Kaciras <Kaciras@protonmail.com>
2022-07-25 20:12:37 +0800

    fix content server url in dev mode

--- src/api/index.ts
+++ src/api/index.ts
@@ -22,13 +22,32 @@ export * from "./notification";
 export * from "./category";
 export * from "./discussion";
 
-const apiOrigin = import.meta.env.API_PUBLIC as any;
-export const contentServiceURL = import.meta.env.SSR
-	? import.meta.env.API_INTERNAL
-	: typeof apiOrigin === "string"
-		? apiOrigin
-		: apiOrigin[location.protocol.substring(0, location.protocol.length - 1)];
+type PublicAPIConfig = string | number | Record<string, string | number>;
 
+/**
+ * 获取内容服务器的 URL 前缀，适配各种场景。
+ *
+ * 因为内容服务器是用 JAVA 写的，我又懒得加层代理去统一域名端口，
+ * 所以在各个情况下其地址不同，复杂到需要一个函数来判断。
+ *
+ * @param config 配置项 config.backend.public
+ */
+function getContentServerURL(config: PublicAPIConfig): string {
+	if (import.meta.env.SSR) {
+		return import.meta.env.API_INTERNAL;
+	}
+	const { protocol, hostname } = location;
+	switch (typeof config) {
+		case "object":
+			return getContentServerURL(config[protocol.slice(0, -1)]);
+		case "string":
+			return config;
+		default:
+			return `${protocol}//${hostname}:${config}`;
+	}
+}
+
+const contentServiceURL = getContentServerURL(import.meta.env.API_PUBLIC);
 const webServiceURL = import.meta.env.SSR ? "__NEVER_USED__" : location.href;
 
 export default defineAPIs({
