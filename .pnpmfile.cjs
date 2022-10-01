ce17ca64ace124a74ad59a15f0e962ce9de35aca
Kaciras <Kaciras@protonmail.com>
2022-10-01 19:41:43 +0800

    update deps

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -5,13 +5,6 @@ function readPackage(packageJson) {
 		dependencies["vue-router"] = "4.0.16"
 	}
 
-	// https://github.com/vuejs/core/issues/6651
-	if (dependencies["vue"]) {
-		dependencies["vue"] = "3.2.37"
-	}
-	if (dependencies["@vue/server-renderer"]) {
-		dependencies["@vue/server-renderer"] = "3.2.37"
-	}
 	return packageJson;
 }
 
