e9cd0543249420cad9d50d7e8ca2a469600165d5
Kaciras <Kaciras@protonmail.com>
2023-02-23 13:55:33 +0800

    bundle public pages into main chunk

--- src/router.ts
+++ src/router.ts
@@ -1,52 +1,37 @@
 import { createMemoryHistory, createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import IndexPage from "./views/index/IndexPage.vue";
+import ListPage from "./views/list/ListPage.vue";
+import LoginPage from "./views/login/LoginPage.vue";
+import ProfilePage from "./views/user/ProfilePage.vue";
 import ErrorPage from "./views/error/ErrorPage.vue";
+import ArticlePage from "./views/article/ArticlePage.vue";
 
-/*
- * Vite 不支持自定义代码分割，如果自定义 manualChunks 的话也没法复用 Vite 内部的逻辑：
- * https://github.com/vitejs/vite/blob/a0126441a556b4991ac14cf037820194ab9e17b9/packages/vite/src/node/build.ts#L619
- *
- * 如果用动态导入，关于页的子路由组件全会被分割，都是很小的文件完全不值得，如果能合并成一个的话还行。
- */
 import AbortPage from "./views/about/AboutPage.vue";
 import MeTab from "./views/about/MeTab.vue";
 import FriendsTab from "./views/about/FriendsTab.vue";
 import TechnologyTab from "./views/about/TechnologyTab.vue";
 import CopyrightTab from "./views/about/CopyrightTab.vue";
 
+/**
+ * 路由表，这里把页面按照对访问速度的敏感性分成了 3 组，越靠前的越重要。
+ */
 const routes: RouteRecordRaw[] = [
+	
+	// 外链入口，是访问频率最高的页面，在优化时要优先考虑。
+	{ path: "/", component: IndexPage },
+	{ path: "/article/:id/:urlTitle", component: ArticlePage },
+	{ path: "/article/:id", component: ArticlePage },
 	{
-		path: "/",
-		component: IndexPage,
+		path: "/error/:value([0-9]+)",
+		props: true,
+		component: ErrorPage,
 	},
-	{
-		path: "/list",
-		redirect: "/list/0",
-	},
-	{
-		path: "/list/:index",
-		component: () => import("./views/list/ListPage.vue"),
-	},
-	{
-		path: "/login",
-		component: () => import("./views/login/LoginPage.vue"),
-	},
-	{
-		path: "/article/:id",
-		component: () => import("./views/article/ArticlePage.vue"),
-	},
-	{
-		path: "/article/:id/:urlTitle",
-		component: () => import("./views/article/ArticlePage.vue"),
-	},
-	{
-		path: "/edit/:draftId",
-		component: () => import("./views/editor/EditorPage.vue"),
-	},
-	{
-		path: "/profile",
-		component: () => import("./views/user/ProfilePage.vue"),
-	},
+
+	// 其它前台页，重要性次之，是对本站感兴趣的用户才会看的，他们的忍耐性更高些。
+	{ path: "/list", redirect: "/list/0" },
+	{ path: "/list/:index", component: ListPage },
+	{ path: "/login", component: LoginPage },
+	{ path: "/profile", component: ProfilePage },
 	{
 		path: "/about",
 		component: AbortPage,
@@ -57,21 +42,22 @@ const routes: RouteRecordRaw[] = [
 			{ path: "copyright", component: CopyrightTab },
 		],
 	},
+
+	// 后台只有管理员才用，没有上面的那么重要了。
+	{
+		path: "/edit/:draftId",
+		component: () => import("./views/editor/EditorPage.vue"),
+	},
 	{
 		path: "/console",
 		component: () => import("./views/console/ConsolePage.vue"),
 		meta: { requireAuth: true },
 	},
-	{
-		path: "/error/:value([0-9]+)",
-		component: ErrorPage,
-		props: true,
-	},
+
 	{
 		/*
-		 * 【新版 vue-router 4 匹配串】
-		 * 最开始的 :pathMatch 是参数，可以随意起名。
-		 * (.*) 表示匹配一个目录内的任意值。
+		 * 【vue-router 4 匹配串】
+		 * 最开始的 :pathMatch 是参数，可以随意起名，(.*) 表示匹配一个目录内的任意值，
 		 * 最后的 * 表示任意次数，加上它就能匹配深层目录。
 		 */
 		path: "/:pathMatch(.*)*",
